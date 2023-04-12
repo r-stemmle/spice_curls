84b6e83e762d46d25b46d53367a38c2714f41e56
Ryan Stemmle <ryan.stemmle@gmail.com>
2023-04-12 16:03:33 -0600

    Create join table for users spices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_215206) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_220135) do
   create_table "spices", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_215206) do
     t.boolean "ground"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "spices_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "spice_id", null: false
+    t.index ["spice_id", "user_id"], name: "index_spices_users_on_spice_id_and_user_id"
+    t.index ["user_id", "spice_id"], name: "index_spices_users_on_user_id_and_spice_id"
   end
 
   create_table "users", force: :cascade do |t|
