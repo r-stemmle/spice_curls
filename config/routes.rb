c5e433e0eb34a80d960cc3fb7933bb5e88b7e77b
Ryan Stemmle <ryan.stemmle@gmail.com>
2023-04-12 16:48:20 -0600

    Change the schema for users and spices

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :spices
+  resources :users do
+    resources :spices, only: [:index, :new, :create]
+  end
+  resources :spices, only: [:show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
